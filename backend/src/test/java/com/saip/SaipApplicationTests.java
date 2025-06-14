9aacdae2cb4768e120ad0701796ed3292775dede
wuliuer <2039900132@qq.com>
2025-06-15 00:31:11 +0800

    11

--- backend/src/test/java/com/saip/SaipApplicationTests.java
+++ backend/src/test/java/com/saip/SaipApplicationTests.java
@@ -1,13 +1,13 @@
-package com.saip;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SaipApplicationTests {
-
-    @Test
-    void contextLoads() {
-        // 测试Spring上下文是否正常加载
-    }
-} 
+//package com.saip;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SaipApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//        // 测试Spring上下文是否正常加载
+//    }
+//}
